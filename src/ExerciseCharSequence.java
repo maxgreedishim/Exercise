0c3e9284b3b12153463fac265ac3fb4e09f72360
maxgreedishim <90276401+maxgreedishim@users.noreply.github.com>
2021-10-18 21:12:55 +0500

    Почему он начинает с последнего? +

--- src/ExerciseCharSequence.java
+++ src/ExerciseCharSequence.java
@@ -1,24 +1,31 @@
 public class ExerciseCharSequence implements CharSequence {
     private String text;
 
+    public String getText(){
+        return text;
+    }
+
     public ExerciseCharSequence(String text) {this.text = text;}
 
     @Override
     public int length() {return text.length();}
 
-    String viceVersa = "";
+    private String viceVersa = "";
+    public String getViceVersa(){
+        return viceVersa;
+    }
 
     @Override
     public char charAt(int index) {
-        for (int i = 0; i < length(); i++) { // Почему он начинает с последнего?
-            viceVersa = text.charAt(i) + viceVersa;
+        for (int i = 0; i < length(); i++) {
+            viceVersa = text.charAt(i) + getViceVersa();
         }
         return 0;
     }
 
     @Override
-    public CharSequence subSequence(int start, int end) {return text;}
+    public CharSequence subSequence(int start, int end) {return "subSequence: " + text;}
 
     @Override
-    public String toString(){return "Это строка: " + viceVersa.toString();}
+    public String toString(){return "Это строка в обратном порядке: " + viceVersa;}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import java.lang.constant.Constable;
-
 public class Main {
 
     public static void main(String[] args) {
         ExerciseCharSequence exerciseCharSequence =
                 new ExerciseCharSequence("Your implementation should return the string backwards.");
+        System.out.println(exerciseCharSequence.getText());
         System.out.println("Колличество символов: " + exerciseCharSequence.length());
-        System.out.println(exerciseCharSequence.subSequence(0,55));
+        System.out.println(exerciseCharSequence.subSequence(0,5));
         exerciseCharSequence.charAt(exerciseCharSequence.length());
-        System.out.println("Это символы в обратном порядке: " + exerciseCharSequence.viceVersa);
-        System.out.println(exerciseCharSequence.toString());
+        System.out.println(exerciseCharSequence);
     }
 }
